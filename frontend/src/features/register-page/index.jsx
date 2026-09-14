832b38d5dc74b1f5948eed086bd70077622415ec
Guacamoleboy <cph-jl325@stud.ek.dk>
2026-09-14 15:14:11 +0200

    Issue #105 - Register frontend solution.

--- frontend/src/features/register-page/index.jsx
+++ frontend/src/features/register-page/index.jsx
@@ -1,3 +1,6 @@
 // Pathing
 // _______
 // src/features/register-page/index.jsx
+
+export { default as RegisterBackground } from '@/features/login-page/background/Background'
+export { default as RegisterCard } from './register/Register'
