d9b124ed31bd083d88b34ac0818e80958f8ea7f9
Guacamoleboy <cph-jl325@stud.ek.dk>
2026-09-14 14:00:01 +0200

    Issue #106 - JWT Implementation

---
+++ backend/src/main/java/alpha/security/jwt/JwtValidator.java
@@ -0,0 +1,33 @@
+package alpha.security.jwt;
+
+import alpha.dao.impl.MemberDAO;
+import alpha.entity.Member;
+import alpha.exception.ApiException;
+import alpha.util.ContextHelper;
+import io.javalin.http.Context;
+
+public class JwtValidator {
+
+    // Attributes
+
+    // _________________________________________________________________________________________________________________
+
+    public static boolean isValid(Context ctx) {
+        String token = ContextHelper.extractBearerToken(ctx);
+        return token != null && JwtUtil.isValid(token);
+    }
+
+    // _________________________________________________________________________________________________________________
+
+    public static Member getMemberFromToken(Context ctx, MemberDAO memberDAO) {
+        String token = ContextHelper.extractBearerToken(ctx);
+        if (!JwtUtil.isValid(token)) {
+            throw new ApiException(401, "Invalid token");
+        }
+        Integer memberId = JwtService.getClaimMemberId(token);
+        Member member = memberDAO.getById(memberId);
+        ContextHelper.notNull(member, "Member");
+        return member;
+    }
+
+}
